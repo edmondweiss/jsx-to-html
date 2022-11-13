6624b932c0a3a2975285a07f582fa8155d678df6
edmondweiss <edmond.weiss@askapplications.com>
2022-11-13 10:02:16 -0300

    Add fragment support

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,103 @@
 type HTML = string;
+type HtmlTagName = string;
+
+type JSXConverter<K extends keyof JSX.IntrinsicElements> = (
+  element: HtmlTagName | JSX.ElementClassConstructor | FunctionComponent,
+  props: Record<string, any>,
+  ...children: string[]
+) => void;
+
+export type FunctionComponent<
+  P extends Record<string, any> = Record<string, any>
+> = (props: P) => JSX.Element;
+
+function isClass(func: unknown): func is JSX.ElementClassConstructor {
+  return (
+    typeof func === "function" &&
+    /^class\s/.test(Function.prototype.toString.call(func))
+  );
+}
+
+// https://developer.mozilla.org/en-US/docs/Glossary/Void_element
+const VOID_ELEMENT_SET = new Set([
+  "area",
+  "base",
+  "br",
+  "col",
+  "embed",
+  "hr",
+  "img",
+  "input",
+  "link",
+  "meta",
+  "param",
+  "source",
+  "track",
+  "wbr",
+]);
+
+const ELEMENT_ATTRIBUTE_EXCLUSION_SET = new Set(["children"]);
+
+function renderProps(props: Record<string, any> | undefined): string {
+  if (!props) {
+    return "";
+  }
+  const attributes = [];
+  for (let [key, value] of Object.entries(props)) {
+    if (ELEMENT_ATTRIBUTE_EXCLUSION_SET.has(key)) {
+      continue;
+    }
+    if (typeof value === "string" || typeof value === "number") {
+      let attr = `${key}="${
+        String(value as number | string)
+          ?.replace(/&/g, "&amp;")
+          .replace(/"/g, "&quot;") ?? ""
+      }"`;
+      attributes.push(attr);
+    } else if (typeof value === "boolean" && value) {
+      attributes.push(key);
+    }
+  }
+  return attributes.join(" ");
+}
+
+export namespace Aui {
+  export const createElement: JSXConverter<keyof JSX.IntrinsicElements> = (
+    element,
+    props,
+    ...children
+  ) => {
+    let html = "";
+    if (typeof element === "string") {
+      html = VOID_ELEMENT_SET.has(element)
+        ? `<${element} ${props ? ` ${renderProps(props)}` : ""}/>`
+        : `<${element}${props ? ` ${renderProps(props)}` : ""}>${children.join(
+            ""
+          )}</${element}>`;
+    } else {
+      if (!props) {
+        props = {};
+      }
+      if (children && children.length) {
+        props.children = children.join("");
+      }
+      if (isClass(element)) {
+        html = new element(props).render();
+      } else {
+        html = element(props);
+      }
+    }
+
+    return html;
+  };
+
+  export const fragment = (props: any) => {
+    if (props == null) {
+      return "";
+    }
+    return props.children; //  props && (props as any).children ? (props as any).children : "";
+  };
+}
 
 declare global {
   namespace JSX {
@@ -49,91 +148,11 @@ declare global {
         | string
         | boolean
         | number
+        | (() => void)
         ? HTMLElementTagNameMap[TagName][Attr]
         : never;
+    } & {
+      children?: TagName extends VOID_ELEMENT ? never : any;
     };
   }
 }
-export type HtmlTagName = string;
-export type FunctionComponent<
-  P extends Record<string, any> = Record<string, any>
-> = (props: P) => JSX.Element;
-
-type JSXConverter<K extends keyof JSX.IntrinsicElements> = (
-  element: HtmlTagName | JSX.ElementClassConstructor | FunctionComponent,
-  props: Record<string, any>,
-  ...children: string[]
-) => void;
-
-// https://developer.mozilla.org/en-US/docs/Glossary/Void_element
-const VOID_ELEMENT_SET = new Set([
-  "area",
-  "base",
-  "br",
-  "col",
-  "embed",
-  "hr",
-  "img",
-  "input",
-  "link",
-  "meta",
-  "param",
-  "source",
-  "track",
-  "wbr",
-]);
-
-function renderProps(props: Record<string, any> | undefined): string {
-  if (!props) {
-    return "";
-  }
-  const attributes = [];
-  for (let [key, value] of Object.entries(props)) {
-    if (key === "children") {
-      continue;
-    }
-    if (typeof value === "string" || typeof value === "number") {
-      let attr = `${key}="${
-        String(value as number | string)
-          ?.replace(/&/g, "&amp;")
-          .replace(/"/g, "&quot;") ?? ""
-      }"`;
-      attributes.push(attr);
-    } else if (typeof value === "boolean" && value) {
-      attributes.push(key);
-    }
-  }
-  return attributes.join(" ");
-}
-
-export const jsxToHtml: JSXConverter<keyof JSX.IntrinsicElements> = (
-  element,
-  props,
-  ...children
-) => {
-  let html = "";
-  if (typeof element === "string") {
-    html = VOID_ELEMENT_SET.has(element)
-      ? `<${element} ${props ? ` ${renderProps(props)}` : ""}/>`
-      : `<${element}${props ? ` ${renderProps(props)}` : ""}>${children.join(
-          ""
-        )}</${element}>`;
-  } else {
-    if (props && children) {
-      props.children = children.join("");
-    }
-    let isClass = false;
-    let component;
-    try {
-      component = new (element as JSX.ElementClassConstructor)();
-      isClass = true;
-      html = component.render();
-    } catch {}
-
-    if (!isClass) {
-      html = (element as FunctionComponent)(props);
-    }
-  }
-
-  return html;
-};
